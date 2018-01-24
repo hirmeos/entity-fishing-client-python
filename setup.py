0ae1f4aa04bbca762ea244503d37baf8f8f1ac64
Luca Foppiano <luca@foppiano.org>
2018-01-24 13:05:25 +0100

    adding more required files

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='entity-fishing-client',
     version='0.2.1',
-    description='A minimal client for the HIRMEOS entity fishing service.',
+    description='A minimal client for entity-fishing service.',
     long_description=long_description,
-    url='https://github.com/ubiquitypress/entity-fishing-client-python',
-    author='Francesco de Virgilio',
-    author_email='francesco.devirgilio@ubiquitypress.com',
+    url='https://github.com/Hirmeos/entity-fishing-client-python',
+    author='Francesco de Virgilio, Luca Foppiano',
+    author_email='francesco.devirgilio@ubiquitypress.com, luca.foppiano@inria.fr',
     license='GNU GPL v3',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -25,7 +25,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
-    keywords='text_processing',
+    keywords=['named entity recognition', 'entity matching', 'entity recognition'],
     install_requires=['requests', 'zenlog'],
     packages=['nerd'],
     package_dir={
