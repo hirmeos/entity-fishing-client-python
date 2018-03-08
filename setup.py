8d09848cd29a05751838e0b1bc83359367e5c205
Luca Foppiano <luca@foppiano.org>
2018-03-08 14:20:34 +0100

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='entity-fishing-client',
-    version='0.2.1',
+    version='0.2.2',
     description='A minimal client for entity-fishing service.',
     long_description=long_description,
     url='https://github.com/Hirmeos/entity-fishing-client-python',
