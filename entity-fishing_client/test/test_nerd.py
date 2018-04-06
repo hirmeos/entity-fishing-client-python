9b91bcd6149948991098a38c693874384c67bda3
Luca Foppiano <luca@foppiano.org>
2018-04-06 16:52:26 +0200

    added unit tests - cannot understand why they are not working

---
+++ entity-fishing_client/test/test_nerd.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class NerdTest(unittest.TestCase):
+
+    def setUp(self):
+        self.target = NerdClient()
+
+    def testDisambiguateText_simpleText(self):
+        self.target.disambiguateText("This is a test which should be somehow working")
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
